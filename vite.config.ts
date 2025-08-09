8d770eb4fa3e3b6ce7609ba0d8d81083c820f391
Khairul <95ahmadkhairul@gmail.com>
2025-08-10 06:23:18 +0700

    add allowed origins *

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   server: {
     port: 5175,
     headers: {
-      "Access-Control-Allow-Origin": getAllowedOrigins(),
+      "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
       "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
     },
