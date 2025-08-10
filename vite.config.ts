589159c435317bc29f7dcc2c9f78400453a4a699
Khairul <95ahmadkhairul@gmail.com>
2025-08-10 07:16:51 +0700

    add allowed origin headers file

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import federation from '@originjs/vite-plugin-federation'
-import { getAllowedOrigins } from './src/utils/origins'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -24,11 +23,6 @@ export default defineConfig({
   ],
   server: {
     port: 5175,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
-    },
   },
   resolve: {
     alias: {
