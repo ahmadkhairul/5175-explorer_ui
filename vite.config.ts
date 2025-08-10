16150f6b21eb349d45be2a5fe2bb64109db0a3cb
Khairul <95ahmadkhairul@gmail.com>
2025-08-10 16:08:03 +0700

    add define

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,11 @@ export default defineConfig({
   server: {
     port: 5175,
   },
+  define: {
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_DEVTOOLS__: false,
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
